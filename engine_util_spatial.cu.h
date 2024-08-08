fad67e609dc1febc23fd94f6c0d651c84b1155eb
Erik Frey <erikfrey@google.com>
2024-08-08 10:06:23 -0700

    fix: upload correct file for engine_util_spatial.cu.h

--- engine_util_spatial.cu.h
+++ engine_util_spatial.cu.h
@@ -1,56 +1,74 @@
-#ifndef MUJOCO_MJX_GOOGLE_CUDA_ENGINE_UTIL_BLAS_CU_H_
-#define MUJOCO_MJX_GOOGLE_CUDA_ENGINE_UTIL_BLAS_CU_H_
+#ifndef MUJOCO_MJX_GOOGLE_CUDA_ENGINE_UTIL_SPATIAL_CU_H_
+#define MUJOCO_MJX_GOOGLE_CUDA_ENGINE_UTIL_SPATIAL_CU_H_
 
 namespace mujoco::mjx::cuda {
 
-inline __device__ void zero(float* x, int n) {
-  memset(x, 0, n * sizeof(float));
+// multiply quaternions
+inline __device__ void mulQuat(float res[4], const float qa[4],
+                               const float qb[4]) {
+  const float tmp[4] = {
+    qa[0]*qb[0] - qa[1]*qb[1] - qa[2]*qb[2] - qa[3]*qb[3],
+    qa[0]*qb[1] + qa[1]*qb[0] + qa[2]*qb[3] - qa[3]*qb[2],
+    qa[0]*qb[2] - qa[1]*qb[3] + qa[2]*qb[0] + qa[3]*qb[1],
+    qa[0]*qb[3] + qa[1]*qb[2] - qa[2]*qb[1] + qa[3]*qb[0]
+  };
+  res[0] = tmp[0];
+  res[1] = tmp[1];
+  res[2] = tmp[2];
+  res[3] = tmp[3];
 }
 
-inline __device__ void copy(float* dst, const float* src, int n) {
-  memcpy(dst, src, n * sizeof(float));
+// rotate vector by quaternion
+inline __device__ void rotVecQuat(float res[3], const float vec[3],
+                                  const float quat[4]) {
+  // tmp = q_w * v + cross(q_xyz, v)
+  const float tmp[3] = {
+    quat[0] * vec[0] + quat[2] * vec[2] - quat[3] * vec[1],
+    quat[0] * vec[1] + quat[3] * vec[0] - quat[1] * vec[2],
+    quat[0] * vec[2] + quat[1] * vec[1] - quat[2] * vec[0]
+  };
+
+  // res = v + 2 * cross(q_xyz, t)
+  res[0] = vec[0] + 2 * (quat[2] * tmp[2] - quat[3] * tmp[1]);
+  res[1] = vec[1] + 2 * (quat[3] * tmp[0] - quat[1] * tmp[2]);
+  res[2] = vec[2] + 2 * (quat[1] * tmp[1] - quat[2] * tmp[0]);
 }
 
-inline __device__ void normalize(float* x, int n) {
-  float norm = 0.0f;
-  for (int i = 0; i < n; ++i) {
-    norm += x[i] * x[i];
-  }
-  norm = sqrt(norm);
-  for (int i = 0; i < n; ++i) {
-    x[i] /= norm;
-  }
+// convert axisAngle to quaternion
+inline __device__ void axisAngle2Quat(float res[4], const float axis[3],
+                                      float angle) {
+  const float s = sin(angle * 0.5);
+  res[0] = cos(angle * 0.5);
+  res[1] = axis[0] * s;
+  res[2] = axis[1] * s;
+  res[3] = axis[2] * s;
 }
 
-// multiply 3-by-3 matrix by vector
-inline __device__ void mulMatVec3(float res[3], const float mat[9],
-                                  const float vec[3]) {
-  res[0] = mat[0] * vec[0] + mat[1] * vec[1] + mat[2] * vec[2];
-  res[1] = mat[3] * vec[0] + mat[4] * vec[1] + mat[5] * vec[2];
-  res[2] = mat[6] * vec[0] + mat[7] * vec[1] + mat[8] * vec[2];
-}
+// convert quaternion to 3D rotation matrix
+inline __device__ void quat2Mat(float res[9], const float quat[4]) {
+  const float q00 = quat[0] * quat[0];
+  const float q01 = quat[0] * quat[1];
+  const float q02 = quat[0] * quat[2];
+  const float q03 = quat[0] * quat[3];
+  const float q11 = quat[1] * quat[1];
+  const float q12 = quat[1] * quat[2];
+  const float q13 = quat[1] * quat[3];
+  const float q22 = quat[2] * quat[2];
+  const float q23 = quat[2] * quat[3];
+  const float q33 = quat[3] * quat[3];
 
-inline __device__ void addTo(float* res, const float* vec, int n) {
-  for (int i = 0; i < n; ++i) {
-    res[i] += vec[i];
-  }
-}
+  res[0] = q00 + q11 - q22 - q33;
+  res[4] = q00 - q11 + q22 - q33;
+  res[8] = q00 - q11 - q22 + q33;
 
-inline __device__ void addToScl(float* res, const float* vec, float scl,
-                                int n) {
-  for (int i = 0; i < n; ++i) {
-    res[i] += vec[i] * scl;
-  }
-}
-
-// res = vec1 - vec2
-inline __device__ void sub(float* res, const float* vec1, const float* vec2,
-                           int n) {
-  for (int i = 0; i < n; ++i) {
-    res[i] = vec1[i] - vec2[i];
-  }
+  res[1] = 2 * (q12 - q03);
+  res[2] = 2 * (q13 + q02);
+  res[3] = 2 * (q12 + q03);
+  res[5] = 2 * (q23 - q01);
+  res[6] = 2 * (q13 - q02);
+  res[7] = 2 * (q23 + q01);
 }
 
 }  // namespace mujoco::mjx::cuda
 
-#endif  // MUJOCO_MJX_GOOGLE_CUDA_ENGINE_UTIL_BLAS_CU_H_
+#endif  // MUJOCO_MJX_GOOGLE_CUDA_ENGINE_UTIL_SPATIAL_CU_H_
